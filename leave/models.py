d136b95bfbe05a2bf222b1a13c527da3a2a2a35e
Brenda Kiptim <kiptim54>
2021-11-17 00:37:34 +0300

    added basic views

--- leave/models.py
+++ leave/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Leave(models.Model):
+    name = models.CharField(max_length=15)
+    days= models.IntegerField()
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
