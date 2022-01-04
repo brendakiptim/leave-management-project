343cee4c3b72960000105368ddde399b3957f03d
Brenda Kiptim <kiptim54>
2022-01-04 21:08:39 +0300

    removed host

--- app/settings.py
+++ app/settings.py
@@ -80,9 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'leavemanagement',
         'USER': 'kiptim',
-        'PASSWORD': 'jerotich',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PASSWORD': 'test',
     }
 }
 
