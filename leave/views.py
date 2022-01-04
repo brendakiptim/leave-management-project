3ffc7d30d57258757da0c4ed97bd16a1a495feb8
Brenda Kiptim <kiptim54>
2022-01-04 21:06:17 +0300

    removed print

--- leave/views.py
+++ leave/views.py
@@ -5,7 +5,6 @@ from .models import Leave, Employee
 
 def index(request):
     employees = Employee.objects.all()
-    print(employees)
     return render(request,"index.html", {'employees':employees})
 
 
