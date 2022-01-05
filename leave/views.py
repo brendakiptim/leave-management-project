0ecf02907600020113984c55a07093f04c526fd3
Brenda Kiptim <kiptim54>
2022-01-05 18:21:27 +0300

    added comments

--- leave/views.py
+++ leave/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Leave, Employee
 
-
+# landing page vier function
 def index(request):
     employees = Employee.objects.all()
     return render(request,"index.html", {'employees':employees})
